50f426035cf691128c4ed81fb2a9e59f5d022cba
arunk13 <arun.infy13@gmail.com>
2016-02-28 01:14:43 -0500

    signalr

---
+++ Signalr.StockTicker/Signalr.StockTicker/Stock.cs
@@ -0,0 +1,43 @@
+﻿namespace Signalr.StockTicker
+{
+    public class Stock
+    {
+        public string Symbol { get; set; }
+
+        public decimal DayOpen { get; private set; }
+
+        private decimal price;
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                if (price == value)
+                    return;
+                price = value;
+                if (DayOpen == 0)
+                    DayOpen = price;
+            }
+        }
+
+        public decimal Change
+        {
+            get
+            {
+                return Price - DayOpen;
+            }
+        }
+
+        public double PercentageChange
+        {
+            get
+            {
+                return (double)System.Math.Round(Change / price, 4);
+            }
+        }
+    }
+}
